6309761e560767c9559feedbf0cc49d36670eb2f
salamaashoush <salamaashoush@gmail.com>
2017-02-20 22:31:39 +0200

    added composer autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,12 +1,6 @@
 <?php
-require 'core/router.php';
-require 'core/request.php';
-
 $app=[];
 $app['config']=require 'config.php';
-require 'core/database/connection.php';
-require 'core/database/QueryBuilder.php';
-
 $app['database']=new QueryBuilder(
     Connection::make($app['config']['database'])
 );
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
 require Router::load('routes.php')->direct(Request::uri(),Request::method());
